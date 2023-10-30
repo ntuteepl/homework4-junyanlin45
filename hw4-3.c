24bfd610c0d1f0c8fac4d1eee3c24b2914d88b6a
junyanlin45 <146065138+junyanlin45@users.noreply.github.com>
2023-10-30 21:51:25 +0800

    Update hw4-3.c

--- hw4-3.c
+++ hw4-3.c
@@ -1,38 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
 int main()
 {
-    int count;
-    scanf("%d", &count);
-    assert( count >= 1 && count <= 10);
-    int data[count][count];
-    for(int i = 0; i < count; i++){
-        for(int j = 0; j < count; j++) scanf("%d", &data[i][j]);
+    int n=0,car,end,temp;
+    int s[24]={0},d[24]={0};
+    while(scanf("%d%d",&s[n],&d[n])!=EOF)
+    {
+       n++;
+    }
+    int ssch[256][512]={0};//每班車的s
+    int dsch[256][512]={0};//每班車的d
+
+    for(int i=0;i<n;i++)//排序
+    {
+        for(int j=i;j<n;j++)
+        {
+         if(s[i]>s[j])
+         {
+          temp=s[j];
+          s[j]=s[i];
+          s[i]=temp;
+          temp=d[j];
+          d[j]=d[i];
+          d[i]=temp;
+         }
+        }
     }
 
-    int max, boy, girl, i, j;
-    for(int k = 0; k < count; k++){
-        max = 0;
-        boy = 0;
-        girl = 0;
-        for(i = 0; i < count; i++){ // 抓最大值
-            for(j = 0; j < count; j++) if(data[i][j] > max) max = data[i][j];
-        }
+    end=0;//上一班車的抵達時間
 
-        for(i = 0; i < count; i++){ // 用最大值判斷輸出
-            for(j = 0; j < count; j++){
-                if(data[i][j] == max){
-                    boy = i;
-                    girl = j;
-                    printf("boy %d pair with girl %d\n", (boy + 1), (girl + 1));
-                }
+    for(int i=0;i<n;i++)//i為車班，到時候去找ssch[]哪個是0就知道有幾班車了
+    {
+        for(int j=0;j<n;j++)//j為那般車的時間表紀錄
+        {
+          for(int k=0;k<n;k++)
+          {
+              if(s[k]>=end && s[k]!=0)//如果發車時間大於上次抵達時間就記錄進時間表內
+              {
+                  end=d[k];
+                  ssch[i][j]=s[k];
+                  dsch[i][j]=d[k];
+                  s[k]=0;
+                  d[k]=0;
+                  j++;
+              }
+          }
+        }
+        end=0;
+    }
+    for(int c=0;c<=n;c++)
+    {
+        if(ssch[c][0]==0)
+        {
+            car=c;
+            printf("%d\n",car);
+            break;
+        }
+    }
+
+    for(int driver = 0; driver < car; driver++){ // 輸出每個駕駛的行程
+        printf("Driver %d's schedule is", driver+1 );
+        for(int f = 0; f < n*2; f++)
+        {
+            if(ssch[driver][f]!=0)
+            {
+                printf(" %d %d",ssch[driver][f],dsch[driver][f]);
+            }
+            else
+            {
+                printf("\n");
+                break;
             }
         }
 
-        for(i = 0; i < count; i++){ // 清除使用過的
-            for(j = 0; j < count; j++) if(i == boy || j == girl) data[i][j] = 0;
-        }
     }
 }
