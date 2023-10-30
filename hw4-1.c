72ad0835eb4e151a3487605770d51dba66909330
junyanlin45 <146065138+junyanlin45@users.noreply.github.com>
2023-10-30 17:19:02 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<assert.h>
+#define ARRAYSIZE 50
+
+int main()
+{
+    int a[ARRAYSIZE];
+    int n;
+
+    scanf("%d",&n);
+    assert(n>0 && n<=ARRAYSIZE);
+
+
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+         int k=0;
+        for(int j=2;j<a[i];j++)
+        {
+            if(a[i]%j==0)
+             {        k=1;
+
+             }
+
+        }
+        if(k==0||a[i]==2)
+            {
+                printf("YES");
+                printf(" ");
+            }
+
+        if(k==1)
+        {
+            printf("NO");
+            printf(" ");
+
+        }
+
+
+
+
+
+
+
+}
+}
 
